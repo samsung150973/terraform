e3220f9ae071ca97c784e25ff3002f27822ff986
unknown <samsung150973@gmail.com>
2023-04-16 20:39:10 +0100

    Change

--- 03-example/ec2.tf
+++ 03-example/ec2.tf
@@ -17,3 +17,30 @@ output "private_dns_of_ec2" {
     value = aws_instance.web.private_dns
   
 }
+
+#create a new security group
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 2
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
