f2e6a5854e7890808edd95b7db64dd01848b2bcf
unknown <samsung150973@gmail.com>
2023-04-16 21:00:37 +0100

    Change

--- 03-example/ec2.tf
+++ 03-example/ec2.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami                   = "ami-004c65616d53d05f6"
   instance_type         = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.b53_allow_ssh_sgm.id]   # Security group is created below. never hard-code 
+  vpc_security_group_ids = [aws_security_group.b53_allow_ssh_sgm.id] 
 
   tags = {
     Name = "Terraform_Instance"
