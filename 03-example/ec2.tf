90bf8659c991166d8a1161ea7ad8a4c41782aecf
unknown <samsung150973@gmail.com>
2023-04-16 20:20:39 +0100

    Change

--- 03-example/ec2.tf
+++ 03-example/ec2.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Bloack to Create an EC2 instance
 resource "aws_instance" "web" {
   ami           = "ami-004c65616d53d05f6"
   instance_type = "t3.micro"
@@ -9,4 +10,10 @@ resource "aws_instance" "web" {
   tags = {
     Name = "Terraform_Instance"
   }
+}
+
+# Block to print Private dns address
+output "private_dns_of_ec2" {
+    value = aws_instance.web.private_dns
+  
 }
